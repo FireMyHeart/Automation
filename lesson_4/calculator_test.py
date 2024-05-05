cca2fdda4f71d2d638665d8a707b6ca5400ef70a
Anna <kraevaannav@gmail.com>
2024-05-05 15:25:05 +0300

    lesson 4

---
+++ lesson_4/calculator_test.py
@@ -0,0 +1,25 @@
+from calculator import Calculator
+import pytest
+
+@pytest.mark.parametrize('num1, num2, result', [(4, 5, 9), (-6, -10, -16), (-6, 6, 0), (5.61, 4.29,  9.9), (35, 0, 35)])
+def test_sum_nums(num1, num2, result):
+    calculator = Calculator()
+    res = calculator.sum(num1, num2)
+    assert res == result
+
+@pytest.mark.parametrize('num1, num2, result', [(10, 2, 5)])
+def test_div_positive_nums(num1, num2, result):
+    calculator = Calculator()
+    res = calculator.div(num1, num2)
+    assert res == result
+
+def test_div_by_zero():
+    calculator = Calculator()
+    with pytest.raises(ArithmeticError):
+        calculator.div(10, 0)
+
+@pytest.mark.parametrize('numbers, result', [([], 0), ([1, 2, 3, 4, 5, 6, 7, 8, 9, 5], 5)])
+def test_avg_list(numbers, result):
+    calculator = Calculator()
+    res = calculator.avg(numbers)
+    assert res == result
